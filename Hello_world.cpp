439786f2d940aae99a2a87153dbbb92f27ca2a56
youtagster51 <youtagster@gmail.com>
2026-01-17 14:42:14 -0800

    Add files via upload

---
+++ Hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+using namespace std;
+    cout << "hello world\n";
+}
+//int is starting point of program, std:: is print to screen,std::end1 ends the commands,
+//# include <iostream is input or output crucial for programs so far (//) allows you to make notes and not disturb the code.
+// { allows this to show the that the main program contains this } << pushes command, ; ends the command,
+// return 0; allos the code and or string to finish saying to errors were made. can also use \n to end command
+// std ;; count tells it to run command, using namespace std; count also allows text to flow.
